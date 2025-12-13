5e8664b306857f2c06a805869497eda1c228f50b
kziou1 <kziou@uth.gr>
2025-12-13 15:12:45 -0500

    Refactor defines.h to add array and identifier structures
    Updated data structures to include array and identifier types.

--- defines.h
+++ defines.h
@@ -1,3 +1,8 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+#define MAX_DIMENSIONS 5
+
 typedef enum {
 	T_CHAR,
 	T_INT,
@@ -6,12 +11,23 @@ typedef enum {
 	T_ID
 } type_t;
 
+typedef struct {
+	int dims;
+	int dim_size[MAX_DIMENSIONS];
+} array_t;
+
+typedef struct {
+	char *id;
+	array_t *arr;
+} id_t;
+
+typedef struct IdList {
+	id_t *id;
+	struct IdList *next;
+} id_list_t;
 
 typedef struct {
 	type_t type ;
 } expr_t;
 
-typedef struct IdList {
-char *id;
-struct IdList *next;
-} id_list_t;
+#endif
